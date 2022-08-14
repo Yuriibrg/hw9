369876d8e4974b745867108f3b6d51c0cba066a2
Yuriibrg <yuriy.topal@gmail.com>
2022-08-14 21:13:39 +0300

    Працюють основні команди

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from parser import parse_command
-from collections import defaultdict
+
 
 BOOK = {}
+
 
 def main():
     user_com = ''
@@ -23,23 +24,46 @@ def user_command_add(user_command):
 
     name = user_command[1]
     number = user_command[2]
-    BOOK[name] = number
+    if not name in BOOK.keys():
+        BOOK[name] = number
+    else:
+        print("Такий контакт вже є у Вашій книзі")
+
     return print(BOOK)
 
 
 
 
 def user_command_change(user_command):
+    name = user_command[1]
+    number = user_command[2]
+    if name in BOOK.keys():
+        BOOK[name] = number
+    else:
+        return print("Такого контакта нема у Вашій книзі")
 
-    pass
+
+
+
+
 
 
 def user_command_phone(user_command):
-    pass
+
+    name = user_command[1]
+    for contact, tel in BOOK.items():
+        if name == contact:
+            print(tel)
+        else:
+           return  print("Такий контакт не зареєстровано у Вашій книзі")
+
 
 
 def user_command_show_all(user_command):
-    pass
+
+    user_command[0] = " ".join(user_command)
+
+    return print(BOOK)
 
 
 def user_command_good_bye(user_command):
@@ -54,6 +78,7 @@ user_commands = {
     "show all": user_command_show_all,
     "good bye": user_command_good_bye,
 }
+
 
 def contact_book(user_command):
 
